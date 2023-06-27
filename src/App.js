c17c96408302399dc3d4badc503ffd1de14a80d0
aware <aware9758@gmail.com>
2023-06-27 16:08:15 +0200

    first commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from "react";
+import "./App.css";
+import Main from "./components/Main";
+export default function App() {
+  return (
+    <div className="title">
+      <h1>Here we go ..</h1>
+      <Main />
+    </div>
+  );
+}
